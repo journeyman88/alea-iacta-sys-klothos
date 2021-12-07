88270198a95f17925e1f4be5377d2f8fa12b7a47
Marco Bignami <m.bignami@unknown-domain.no-ip.net>
2021-12-07 14:26:38 +0100

    Localized results

--- src/main/java/net/unknowndomain/alea/systems/klothos/KlothosResults.java
+++ src/main/java/net/unknowndomain/alea/systems/klothos/KlothosResults.java
@@ -21,14 +21,16 @@ import java.util.List;
 import net.unknowndomain.alea.messages.MsgBuilder;
 import net.unknowndomain.alea.messages.MsgStyle;
 import net.unknowndomain.alea.random.SingleResult;
-import net.unknowndomain.alea.roll.GenericResult;
+import net.unknowndomain.alea.roll.LocalizedResult;
 
 /**
  *
  * @author journeyman
  */
-public class KlothosResults extends GenericResult
+public class KlothosResults extends LocalizedResult
 {
+    private final static String BUNDLE_NAME = "net.unknowndomain.alea.systems.klothos.RpgSystemBundle";
+    
     private final List<SingleResult<Integer>> actionResults;
     private final SingleResult<Integer> fateDice;
     private int total = 0;
@@ -62,20 +64,20 @@ public class KlothosResults extends GenericResult
     protected void formatResults(MsgBuilder messageBuilder, boolean verbose, int indentValue)
     {
         String indent = getIndent(indentValue);
-        messageBuilder.append("Result: ").append(total);
+        messageBuilder.append(translate("klothos.results.total", total));
         if (misfortune > 0)
         {
-            messageBuilder.append(" | Misfortune : ").append(misfortune, MsgStyle.BOLD);
+            messageBuilder.append(" | ").append(translate("klothos.results.misfortune", misfortune), MsgStyle.BOLD);
         }
         if (luck > 0)
         {
-            messageBuilder.append(" | Luck : ").append(luck, MsgStyle.BOLD);
+            messageBuilder.append(" | ").append(translate("klothos.results.luck", luck), MsgStyle.BOLD);
         }
         messageBuilder.appendNewLine();
         if (verbose)
         {
             messageBuilder.append(indent).append("Roll ID: ").append(getUuid()).appendNewLine();
-            messageBuilder.append("Dice Results: ").append(" [ ");
+            messageBuilder.append(translate("klothos.results.diceResults")).append(" [ ");
             for (SingleResult<Integer> t : getActionResults())
             {
                 messageBuilder.append("( ").append(t.getLabel()).append(" => ");
@@ -84,7 +86,7 @@ public class KlothosResults extends GenericResult
             messageBuilder.append("]").appendNewLine();
             if (fateDice != null)
             {
-                messageBuilder.append("Fate Dice: ").append(" [ ");
+                messageBuilder.append(translate("klothos.results.fateDice")).append(" [ ");
                 messageBuilder.append(fateDice.getLabel()).append(" => ");
                 messageBuilder.append(fateDice.getValue());
                 messageBuilder.append(" ]").appendNewLine();
@@ -100,6 +102,12 @@ public class KlothosResults extends GenericResult
     public void setTotal(int total)
     {
         this.total = total;
+    }
+
+    @Override
+    protected String getBundleName()
+    {
+        return BUNDLE_NAME;
     }
 
 }
